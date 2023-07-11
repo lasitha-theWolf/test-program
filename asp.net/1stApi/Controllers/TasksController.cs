190f435f7c77a28edb844b70d003a526873729d3
Lasitha Heenkenda <Lasithaheenkenda23@gmail.com>
2023-07-11 12:50:12 +0530

    update

---
+++ asp.net/1stApi/Controllers/TasksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1stApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Tasks()
+        {
+            var tasks = new String[] { "Task 1", "Task 2", "task 3" };
+            return Ok(tasks);
+        }
+
+        [HttpPost]
+
+        public IActionResult NewTasks() {
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            var somthingWentWrong = true;
+
+            if (somthingWentWrong)
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
