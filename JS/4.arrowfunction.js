ecce457f00f51b3678db7252fc26e74d0c50a998
Lasitha Heenkenda <Lasithaheenkenda23@gmail.com>
2023-07-21 01:00:48 +0530

    arrowfunction

---
+++ JS/4.arrowfunction.js
@@ -0,0 +1,37 @@
+// function normal(a,b){
+//     return a+b;
+// }
+
+// console.log("Result : ", normal(1,2));
+
+
+
+
+//-------------------------------------------------
+
+// const add = (a,b) => {
+//     return a+b;
+// }
+
+//console.log("result", add(1,5));
+
+
+
+
+//-----------------------------------------------
+
+const add = (a,b) => a+b;  
+
+const multpile = x => x*x;
+
+console.log("multiply", multpile(5));
+console.log("result", add(1,5));
+
+
+//------------anonymous function-------------------
+
+setTimeout(()=>{
+    console.log("After 3s.. ")
+}, 3000);
+
+
